93fd087603b233123254804b8512d74d4970255d
nenahui <kanatraccoon@gmail.com>
2024-07-04 17:16:23 +0600

    Init app

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Theme appearance={'dark'}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
+  </React.StrictMode>
+);
